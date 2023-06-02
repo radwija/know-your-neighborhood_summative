6ee2dc815541f400eda4545a03cd68ba67318359
radwija <115165737+radwija@users.noreply.github.com>
2023-06-02 12:41:57 +0800

    changing package name

---
+++ src/main/java/com/radwija/springsocial/controller/AuthController.java
@@ -0,0 +1,82 @@
+package com.radwija.springsocial.controller;
+
+import com.radwija.springsocial.exception.BadRequestException;
+import com.radwija.springsocial.model.AuthProvider;
+import com.radwija.springsocial.model.User;
+import com.radwija.springsocial.payload.ApiResponse;
+import com.radwija.springsocial.payload.AuthResponse;
+import com.radwija.springsocial.payload.LoginRequest;
+import com.radwija.springsocial.payload.SignUpRequest;
+import com.radwija.springsocial.repository.UserRepository;
+import com.radwija.springsocial.security.TokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = tokenProvider.createToken(authentication);
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new BadRequestException("Email address already in use.");
+        }
+
+        // Creating user's account
+        User user = new User();
+        user.setName(signUpRequest.getName());
+        user.setEmail(signUpRequest.getEmail());
+        user.setPassword(signUpRequest.getPassword());
+        user.setProvider(AuthProvider.local);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User result = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/user/me")
+                .buildAndExpand(result.getId()).toUri();
+
+        return ResponseEntity.created(location)
+                .body(new ApiResponse(true, "User registered successfully@"));
+    }
+
+}
---
+++ src/main/java/com/radwija/springsocial/model/AuthProvider.java
@@ -0,0 +1,8 @@
+package com.radwija.springsocial.model;
+
+public enum  AuthProvider {
+    local,
+    facebook,
+    google,
+    github
+}
