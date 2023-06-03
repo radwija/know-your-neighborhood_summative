6bf3225c517ba5569c37b8264e72e6336f9d354c
radwija <115165737+radwija@users.noreply.github.com>
2023-06-03 14:58:49 +0800

    fixing role login

--- src/main/java/com/radwija/knowyourneighborhood/controller/UserController.java
+++ src/main/java/com/radwija/knowyourneighborhood/controller/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/user/me")
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
     public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
         return userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
